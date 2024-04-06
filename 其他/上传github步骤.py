c774ed36f9d19a94f2888a6a547ea2a802e337c3
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-04-06 14:42:29 +0800

    1.新增其他目录

---
+++ 其他/上传github步骤.py
@@ -0,0 +1,8 @@
+"""
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/liqiu123456123/pyqt_video-player.git
+git push -u origin main
+"""
