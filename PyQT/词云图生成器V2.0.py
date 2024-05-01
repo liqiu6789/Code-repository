323376513e3b9f459cf54bc661ed6e6f5b716ed0
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-05-01 12:21:03 +0800

    词云图生成器2.0

--- PyQT/词云图生成器V2.0.py
+++ PyQT/词云图生成器V2.0.py
@@ -1,25 +1,44 @@
+"""
+楷体：simkai.ttf
+隶书：SIMLI.TTF
+宋体:simsun.ttc
+黑体：simhei.ttf
+微软雅黑：msyh.ttc
+
+
+"""
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSizePolicy, QFileDialog, \
-    QColorDialog
+    QColorDialog, QComboBox
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 from wordcloud import WordCloud
 import jieba
+import numpy as np
+from PIL import Image
+
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.color_code = None
+        self.mask_shape = None
+        self.ch_font = None
         self.initUI()
 
     def initUI(self):
+        self.font_dict = {'楷体': 'simkai.ttf', '隶书': 'SIMLI.TTF', '宋体': 'simsun.ttc', '黑体': 'simhei.ttf',
+                          '微软雅黑': 'msyh.ttc'}
         # 创建布局
         vbox = QVBoxLayout()
-
         # 第一部分：横向排列的四个按钮
-        button_list = ["选择文本", "选择停用词", "选择背景颜色", "导出词云图"]
+        button_list = ["选择文本", "选择停用词", "选择背景颜色", "导出词云图", "选择词云图形状", "选择字体"]
+        self.font_type = QComboBox(self)
+        # 遍历字典的键，并将它们添加到 QComboBox 中
+        for key in self.font_dict:
+            self.font_type.addItem(key)
         hbox1 = QHBoxLayout()
-        for i in range(4):
+        for i in range(6):
             btn = QPushButton(button_list[i])
             if button_list[i] == "选择文本":
                 btn.clicked.connect(self.openTextFile)
@@ -28,9 +47,13 @@ class MainWindow(QWidget):
             elif button_list[i] == "选择背景颜色":
                 btn.setObjectName("选择背景颜色")
                 btn.clicked.connect(self.selectBackgroundColor)
+            elif button_list[i] == "选择词云图形状":
+                btn.setObjectName("选择词云图形状")
+                btn.clicked.connect(self.open_mask_img)
             else:
                 btn.clicked.connect(self.save_wordcloud_image)
             hbox1.addWidget(btn)
+        hbox1.addWidget(self.font_type)
         vbox.addLayout(hbox1)
 
         # 第二部分：一个按钮
@@ -45,9 +68,18 @@ class MainWindow(QWidget):
         vbox.addWidget(self.canvas)  # 将画布添加到布局中
         # 设置窗口属性
         self.setLayout(vbox)
-        self.setWindowTitle('词云图生成器V1.0')
+        self.setWindowTitle('词云图生成器V2.0')
         self.setGeometry(500, 300, 800, 640)
         self.show()
+
+    def open_mask_img(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "选择词云图形状", "",
+                                                  "Image Files (*.png *.xpm *.jpg *.jpeg)",
+                                                  options=options)
+        if fileName:
+            self.mask_shape = np.array(Image.open(fileName))
 
     def save_wordcloud_image(self):
         options = QFileDialog.Options()
@@ -58,14 +90,22 @@ class MainWindow(QWidget):
         if fileName:
             self.fig.savefig(fileName, dpi=100, bbox_inches='tight')
             print(f"词云图已保存为 {fileName}")
+
     def update_wordcloud(self):
+        # 字体选择
+        selected_text = self.font_type.currentText()
+        # 使用字典的键来检索对应的值
+        self.ch_font = self.font_dict[selected_text]
         # 使用jieba进行分词
         word_list = jieba.cut(self.text, cut_all=False)
         words = " ".join(word_list)
         # 设置matplotlib使用支持中文的字体
         # 创建WordCloud对象并生成词云图
         print(self.color_code)
-        wordcloud = WordCloud(width=800, height=800, background_color=self.color_code if self.color_code is not None else "white", stopwords=None,font_path='msyh.ttc',).generate(
+        wordcloud = WordCloud(width=800, height=800,
+                              background_color=self.color_code if self.color_code is not None else "white", \
+                              stopwords=None, font_path=self.ch_font if self.ch_font is not None else 'msyh.ttc',
+                              mask=self.mask_shape if self.mask_shape is not None else None).generate(
             words)
 
         # 清除之前的图像并绘制新的词云图
@@ -103,7 +143,6 @@ class MainWindow(QWidget):
             # 更新按钮文本颜色
             button.setStyleSheet(f"color: {self.color_code};")
             # 这里添加使用所选颜色的逻辑
-
 
 
 if __name__ == '__main__':
