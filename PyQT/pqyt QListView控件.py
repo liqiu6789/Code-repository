3328bc6d7365a3cc0c4d0c58167bb28fa74c2171
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-05-14 22:49:42 +0800

    更新pyqt QListView控件代码

--- PyQT/pqyt QListView控件.py
+++ PyQT/pqyt QListView控件.py
@@ -1,31 +1,50 @@
-from PyQt5.QtWidgets import QApplication, QListView, QVBoxLayout, QWidget
-from PyQt5.QtCore import QStringListModel
+from PyQt5.QtWidgets import QApplication, QListView, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtCore import Qt
 
 
 class ListViewExample(QWidget):
     def __init__(self):
         super().__init__()
 
+        # 初始化UI
+        self.initUI()
+
+    def initUI(self):
         # 设置布局
         layout = QVBoxLayout(self)
 
-        # 创建一个字符串列表
-        string_list = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5']
+        # 创建一个模型
+        self.model = QStandardItemModel(0, 1)  # 0行1列
 
-        # 创建一个 QStringListModel 并设置数据
-        model = QStringListModel()
-        model.setStringList(string_list)
+        # 添加一些数据
+        for i in range(50):  # 假设有50条数据
+            item = QStandardItem(f"Item {i + 1}")
+            self.model.appendRow(item)
 
-        # 创建一个 QListView 并设置模型
-        list_view = QListView()
-        list_view.setModel(model)
+            # 设置模型可以排序
+        self.model.setSortRole(Qt.DisplayRole)
+        self.model.sort(0, Qt.AscendingOrder)
 
-        # 添加 QListView 到布局中
-        layout.addWidget(list_view)
+        # 创建一个视图并设置模型
+        self.view = QListView()
+        self.view.setModel(self.model)
+
+        # 添加视图到布局
+        layout.addWidget(self.view)
+
+        # 可选：添加一个按钮以触发排序
+        sort_button = QPushButton("Sort Descending")
+        sort_button.clicked.connect(self.sortDescending)
+        layout.addWidget(sort_button)
 
         # 设置窗口标题和大小
-        self.setWindowTitle('QListView Example')
-        self.setGeometry(100, 100, 300, 200)
+        self.setWindowTitle('QListView Example with Sorting')
+        self.setGeometry(100, 100, 300, 300)
+
+    def sortDescending(self):
+        # 根据第一列降序排序
+        self.model.sort(0, Qt.DescendingOrder)
 
 
 if __name__ == '__main__':
