8f54ee892edf6bfc7c9a5ec2b05342ba12b929e6
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-05-31 22:52:58 +0800

    代码优化

--- 其他/wordhelper/listWindow.py
+++ 其他/wordhelper/listWindow.py
@@ -6,61 +6,10 @@ class Ui_ListWindow(object):
         ListWindow.resize(800, 588)
         self.centralwidget = QtWidgets.QWidget(ListWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.horizontalLayoutWidget_3 = QtWidgets.QWidget(self.centralwidget)
-        self.horizontalLayoutWidget_3.setGeometry(QtCore.QRect(20, 443, 761, 31))
-        self.horizontalLayoutWidget_3.setObjectName("horizontalLayoutWidget_3")
-        self.horizontalLayout_3 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_3)
-        self.horizontalLayout_3.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_3.addItem(spacerItem)
-        self.checkBox = QtWidgets.QCheckBox(self.horizontalLayoutWidget_3)
-        self.checkBox.setObjectName("checkBox")
-        self.horizontalLayout_3.addWidget(self.checkBox)
-        self.executeButton = QtWidgets.QPushButton(self.horizontalLayoutWidget_3)
-        self.executeButton.setObjectName("executeButton")
-        self.horizontalLayout_3.addWidget(self.executeButton)
-        self.groupBox = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox.setGeometry(QtCore.QRect(20, 10, 761, 431))
-        self.groupBox.setObjectName("groupBox")
-        self.horizontalLayoutWidget = QtWidgets.QWidget(self.groupBox)
-        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(20, 20, 721, 31))
-        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
-        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
-        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
-        self.label.setObjectName("label")
-        self.horizontalLayout.addWidget(self.label)
-        self.sourcepath = QtWidgets.QLineEdit(self.horizontalLayoutWidget)
-        self.sourcepath.setObjectName("sourcepath")
-        self.horizontalLayout.addWidget(self.sourcepath)
-        self.browseButton = QtWidgets.QToolButton(self.horizontalLayoutWidget)
-        self.browseButton.setObjectName("browseButton")
-        self.horizontalLayout.addWidget(self.browseButton)
-        self.listword = QtWidgets.QListWidget(self.groupBox)
-        self.listword.setGeometry(QtCore.QRect(20, 70, 721, 341))
-        self.listword.setObjectName("listword")
-        self.groupBox_2 = QtWidgets.QGroupBox(self.centralwidget)
-        self.groupBox_2.setGeometry(QtCore.QRect(20, 470, 761, 81))
-        self.groupBox_2.setObjectName("groupBox_2")
-        self.horizontalLayoutWidget_2 = QtWidgets.QWidget(self.groupBox_2)
-        self.horizontalLayoutWidget_2.setGeometry(QtCore.QRect(20, 30, 721, 31))
-        self.horizontalLayoutWidget_2.setObjectName("horizontalLayoutWidget_2")
-        self.horizontalLayout_2 = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget_2)
-        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout_2.addWidget(self.label_2)
-        self.listfile = QtWidgets.QLabel(self.horizontalLayoutWidget_2)
-        self.listfile.setObjectName("listfile")
-        self.horizontalLayout_2.addWidget(self.listfile)
-        spacerItem1 = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
-        self.horizontalLayout_2.addItem(spacerItem1)
-        self.openButton = QtWidgets.QPushButton(self.horizontalLayoutWidget_2)
-        self.openButton.setObjectName("openButton")
-        self.horizontalLayout_2.addWidget(self.openButton)
+
+        self._setup_layouts()
+        self._setup_widgets()
+
         ListWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(ListWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 18))
@@ -73,10 +22,91 @@ class Ui_ListWindow(object):
         self.retranslateUi(ListWindow)
         QtCore.QMetaObject.connectSlotsByName(ListWindow)
 
+    def _setup_layouts(self):
+        self.horizontalLayoutWidget_3 = self._create_layout_widget(self.centralwidget, 20, 443, 761, 31)
+        self.horizontalLayout_3 = self._create_hbox_layout(self.horizontalLayoutWidget_3)
+        self.horizontalLayout_3.addItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum))
+
+        self.groupBox = self._create_group_box(self.centralwidget, 20, 10, 761, 431)
+        self.horizontalLayoutWidget = self._create_layout_widget(self.groupBox, 20, 20, 721, 31)
+        self.horizontalLayout = self._create_hbox_layout(self.horizontalLayoutWidget)
+
+        self.groupBox_2 = self._create_group_box(self.centralwidget, 20, 470, 761, 81)
+        self.horizontalLayoutWidget_2 = self._create_layout_widget(self.groupBox_2, 20, 30, 721, 31)
+        self.horizontalLayout_2 = self._create_hbox_layout(self.horizontalLayoutWidget_2)
+        self.horizontalLayout_2.addItem(QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum))
+
+    def _setup_widgets(self):
+        self.checkBox = self._create_check_box(self.horizontalLayoutWidget_3, "checkBox", self.horizontalLayout_3)
+        self.executeButton = self._create_push_button(self.horizontalLayoutWidget_3, "executeButton", self.horizontalLayout_3)
+
+        self.label = self._create_label(self.horizontalLayoutWidget, "label", self.horizontalLayout)
+        self.sourcepath = self._create_line_edit(self.horizontalLayoutWidget, "sourcepath", self.horizontalLayout)
+        self.browseButton = self._create_tool_button(self.horizontalLayoutWidget, "browseButton", self.horizontalLayout)
+        self.listword = self._create_list_widget(self.groupBox, 20, 70, 721, 341, "listword")
+
+        self.label_2 = self._create_label(self.horizontalLayoutWidget_2, "label_2", self.horizontalLayout_2)
+        self.listfile = self._create_label(self.horizontalLayoutWidget_2, "listfile", self.horizontalLayout_2)
+        self.openButton = self._create_push_button(self.horizontalLayoutWidget_2, "openButton", self.horizontalLayout_2)
+
+    def _create_layout_widget(self, parent, x, y, width, height):
+        widget = QtWidgets.QWidget(parent)
+        widget.setGeometry(QtCore.QRect(x, y, width, height))
+        widget.setObjectName(f"horizontalLayoutWidget_{x}_{y}")
+        return widget
+
+    def _create_hbox_layout(self, widget):
+        layout = QtWidgets.QHBoxLayout(widget)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.setObjectName(f"horizontalLayout_{widget.objectName()}")
+        return layout
+
+    def _create_group_box(self, parent, x, y, width, height):
+        group_box = QtWidgets.QGroupBox(parent)
+        group_box.setGeometry(QtCore.QRect(x, y, width, height))
+        group_box.setObjectName(f"groupBox_{x}_{y}")
+        return group_box
+
+    def _create_check_box(self, parent, name, layout):
+        check_box = QtWidgets.QCheckBox(parent)
+        check_box.setObjectName(name)
+        layout.addWidget(check_box)
+        return check_box
+
+    def _create_push_button(self, parent, name, layout):
+        button = QtWidgets.QPushButton(parent)
+        button.setObjectName(name)
+        layout.addWidget(button)
+        return button
+
+    def _create_label(self, parent, name, layout):
+        label = QtWidgets.QLabel(parent)
+        label.setObjectName(name)
+        layout.addWidget(label)
+        return label
+
+    def _create_line_edit(self, parent, name, layout):
+        line_edit = QtWidgets.QLineEdit(parent)
+        line_edit.setObjectName(name)
+        layout.addWidget(line_edit)
+        return line_edit
+
+    def _create_tool_button(self, parent, name, layout):
+        tool_button = QtWidgets.QToolButton(parent)
+        tool_button.setObjectName(name)
+        layout.addWidget(tool_button)
+        return tool_button
+
+    def _create_list_widget(self, parent, x, y, width, height, name):
+        list_widget = QtWidgets.QListWidget(parent)
+        list_widget.setGeometry(QtCore.QRect(x, y, width, height))
+        list_widget.setObjectName(name)
+        return list_widget
+
     def retranslateUi(self, ListWindow):
         _translate = QtCore.QCoreApplication.translate
         ListWindow.setWindowTitle(_translate("ListWindow", "提取总目录"))
-        self.checkBox.setText(_translate("ListWindow", "包含页码      "))
+        self.checkBox.setText(_translate("ListWindow", "包含页码"))
         self.executeButton.setText(_translate("ListWindow", "开始提取"))
         self.groupBox.setTitle(_translate("ListWindow", "源"))
         self.label.setText(_translate("ListWindow", "请选择Word文档所在目录："))
@@ -85,4 +115,3 @@ class Ui_ListWindow(object):
         self.label_2.setText(_translate("ListWindow", "目录文件保存位置："))
         self.listfile.setText(_translate("ListWindow", "还未提取..."))
         self.openButton.setText(_translate("ListWindow", "打开文件"))
-
--- 其他/wordhelper/wordhelper.py
+++ 其他/wordhelper/wordhelper.py
@@ -1,32 +1,36 @@
-import sys  # 操作系统模块
-from PyQt5.QtGui import  QColor  # 导入PyQt5的QtGui模块
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QTableWidget, QMessageBox # 导入PyQt5的QtWidgets模块
-import os  # 导入操作系统模块
-import tools.common as common  # 导入工具模块并设置别名为common
+import sys
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QTableWidget, \
+    QMessageBox
+import os
+import tools.common as common
 import tools.wordtopdf as wordtopdf
 import tools.mergepdf as mergepdf
-from mainWindow import *   # 导入主窗体的UI类
-from pageWindow import *  # 导入Word转PDF窗体的UI类
-from listWindow import *  # 导入统计Word文档页码窗体的UI类
-from transformWindow import *  # 导入提取总页码窗体的UI类
+from mainWindow import *
+from pageWindow import *
+from listWindow import *
+from transformWindow import *
 import _thread
+
+
 # 主窗体初始化类
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        super(MyMainWindow,self).__init__()
+        super(MyMainWindow, self).__init__()
         self.setupUi(self)
         self.setGeometry(100, 100, 1024, 600)
         self.setWindowTitle('文档处理系统')  # 设置窗体的标题
         # 设置窗体背景
         palette = QtGui.QPalette()  # 创建调色板类的对象
         # 设置窗体背景自适应
-        palette.setBrush(self.backgroundRole(),QBrush(QPixmap("./image/bg.png").scaled(self.size(),QtCore.Qt.IgnoreAspectRatio,QtCore.Qt.SmoothTransformation)))
+        palette.setBrush(self.backgroundRole(), QBrush(
+            QPixmap("./image/bg.png").scaled(self.size(), QtCore.Qt.IgnoreAspectRatio, QtCore.Qt.SmoothTransformation)))
         self.setPalette(palette)
-        self.setAutoFillBackground(True) # 设置自动填充背景
-        self.setFixedSize(1024,600)  # 禁止显示最大化按钮及调整窗体大小
+        self.setAutoFillBackground(True)  # 设置自动填充背景
+        self.setFixedSize(1024, 600)  # 禁止显示最大化按钮及调整窗体大小
 
 
-'''Word转PDF模块'''
+
 class TransformWindow(QMainWindow, Ui_TransformWindow):
     filelist = []
 
@@ -113,128 +117,131 @@ class TransformWindow(QMainWindow, Ui_TransformWindow):
             for file in valueList:
                 os.remove(file)
 
-
-'''统计Word文档页码模块'''
 class PageWindow(QMainWindow, Ui_PageWindow):
+    filelist = []
 
-    filelist = []    # Word文件路径列表
     def __init__(self):
-        super(PageWindow,self).__init__()
+        super(PageWindow, self).__init__()
         self.setupUi(self)
-        self.pagetable.setColumnWidth(0,600)  # 设置第一列的宽度
-        self.pagetable.setColumnWidth(1,100)  # 设置第二列的宽度
-        self.pagetable.setStyleSheet("background-color: lightblue"
-                                     "(spread:pad,stop:0.823 rgba(255, 255, 255, 204), stop:1 rgba(255, 255, 255, 204));"
+        self._setup_ui()
+        self._connect_signals()
+
+    def _setup_ui(self):
+        self.pagetable.setColumnWidth(0, 600)
+        self.pagetable.setColumnWidth(1, 100)
+        self.pagetable.setStyleSheet("background-color: lightblue;"
                                      "selection-background-color:lightblue;")
-        headItem = self.pagetable.horizontalHeaderItem(0)  # 获得水平方向表头的Item对象
-        headItem.setBackground(QColor(0, 60, 10))  # 设置单元格背景颜色
-        headItem.setForeground(QColor(200, 111, 30))  # 设置文字颜色
-        headItem = self.pagetable.horizontalHeaderItem(1)  # 获得水平方向表头的Item对象
-        headItem.setBackground(QColor(0, 60, 10))  # 设置单元格背景颜色
-        headItem.setForeground(QColor(200, 111, 30))  # 设置文字颜色
+        self._style_header(self.pagetable.horizontalHeaderItem(0))
+        self._style_header(self.pagetable.horizontalHeaderItem(1))
         self.pagetable.setEditTriggers(QTableWidget.NoEditTriggers)
         self.pagetable.setSelectionBehavior(QTableWidget.SelectRows)
         self.pagetable.setSelectionMode(QTableWidget.SingleSelection)
         self.pagetable.setAlternatingRowColors(True)
-        self.totalpage.setMinimumWidth(100)  # 设置Label标签的最小宽度
+        self.totalpage.setMinimumWidth(100)
 
-        self.browseButton.clicked.connect(self.sourcebrowseClick)  # 选择源路径
-        self.executeButton.clicked.connect(self.executeClick)  # 开始统计按钮的事件绑定
+    def _style_header(self, header_item):
+        header_item.setBackground(QColor(0, 60, 10))
+        header_item.setForeground(QColor(200, 111, 30))
 
-    # 自定义打开子窗体的方法
+    def _connect_signals(self):
+        self.browseButton.clicked.connect(self.sourcebrowseClick)
+        self.executeButton.clicked.connect(self.executeClick)
+
     def open(self):
         self.__init__()
-        self.show()  # 显示子窗体
-
+        self.show()
 
     def sourcebrowseClick(self):
         dir_path = QFileDialog.getExistingDirectory(self, "请选择源文件目录", r"E:\learn\test\doc")
-        if dir_path != "":  # 判断已经选择了源文件目录
+        if dir_path:
             self.sourcepath.setText(dir_path)
-            self.listword.clear()   # 清空列表
+            self.listword.clear()
             global filelist
-            filelist = common.getfilenames(dir_path,[],'.doc')  # 获取Word文档
+            filelist = common.getfilenames(dir_path, [], '.doc')
             self.listword.addItems(filelist)
 
-    def executeClick(self):  #开始统计按钮的自定义事件
+    def executeClick(self):
         if self.listword.count() == 0:
             QMessageBox.information(self, "温馨提示：", "没有要统计页码的Word文档！", QMessageBox.Yes)
             return
+        self._start_processing()
+
+    def _start_processing(self):
         self.totalpage.setText("")
-        self.totalpage.setMovie(self.gif)  # 设置gif图片
+        self.totalpage.setMovie(self.gif)
         self.label_2.setText("正在统计：")
-        self.gif.start()  # 启动图片，实现等待gif图片的显示
-        _thread.start_new_thread(self.execute,())  # 开启新线程执行统计页码
-    # 统计页码
+        self.gif.start()
+        _thread.start_new_thread(self.execute, ())
+
     def execute(self):
-        valueList = []
         valueList = wordtopdf.wordtopdf1(filelist)
-        # if valueList != []:
-        #     self.totalpage.clear()  # 转换完毕就将等待gif图片清理掉
-        totalPages = str(valueList[0]) # 总页数
+        totalPages = str(valueList[0])
         self.label_2.setText("合计页码：")
-        self.totalpage.setText(totalPages)   # 显示统计出来的页码
-        print("行数：",len(valueList[1]))
-        self.pagetable.setRowCount(len(valueList[1]))  # 指定行数
-        resultList = valueList[1]  # 获取统计结果
-        for i in range(self.pagetable.rowCount()):
-            for j in range(self.pagetable.columnCount()):
-                content = resultList[i][j]  # 获取一个单元格的内容
-                newItem = QTableWidgetItem(content)  # 转换为一个单元格对象
-                self.pagetable.setItem(i, j, newItem)  # 显示在单元格中
+        self.totalpage.setText(totalPages)
+        self._populate_table(valueList[1])
+
+    def _populate_table(self, resultList):
+        self.pagetable.setRowCount(len(resultList))
+        for i, row in enumerate(resultList):
+            for j, content in enumerate(row):
+                self.pagetable.setItem(i, j, QTableWidgetItem(content))
+
 
 '''提取总目录模块'''
+
+
 class ListWindow(QMainWindow, Ui_ListWindow):
     def __init__(self):
-        super(ListWindow,self).__init__()
+        super(ListWindow, self).__init__()
         self.setupUi(self)
-        self.browseButton.clicked.connect(self.sourcebrowseClick)   # 选择源路径
-        self.executeButton.clicked.connect( self.getListClick)  #按钮事件绑定
+        self.browseButton.clicked.connect(self.sourcebrowseClick)  # 选择源路径
+        self.executeButton.clicked.connect(self.getListClick)  # 按钮事件绑定
         self.openButton.clicked.connect(self.openButtonClick)  # 为打开文件按钮绑定事件
+
     # 自定义打开子窗体的方法
     def open(self):
         self.__init__()
         self.show()  # 显示子窗体
+
     def sourcebrowseClick(self):
         dir_path = QFileDialog.getExistingDirectory(self, "请选择源文件目录", r"E:\learn\test\doc")
         if dir_path != "":  # 判断已经选择了源文件目录
             self.sourcepath.setText(dir_path)
-            self.listword.clear()   # 清空列表
+            self.listword.clear()  # 清空列表
             global filelist
-            filelist = common.getfilenames(dir_path,[],'.doc')  # 获取Word文档
+            filelist = common.getfilenames(dir_path, [], '.doc')  # 获取Word文档
             self.listword.addItems(filelist)
 
-    def getListClick(self):  #子窗体自定义事件
+    def getListClick(self):  # 子窗体自定义事件
         if self.listword.count() == 0:
             QMessageBox.information(self, "温馨提示：", "没有要提取目录的Word文档！", QMessageBox.Yes)
             return
         self.listfile.setText("")
         self.listfile.setMovie(self.gif)  # 设置gif图片
         self.gif.start()  # 启动图片，实现等待gif图片的显示
-        _thread.start_new_thread(self.getList,())  # 开启新线程执行统计页码
+        _thread.start_new_thread(self.getList, ())  # 开启新线程执行统计页码
 
-     # 提取目录
+    # 提取目录
     def getList(self):
-        sourcepath = self. sourcepath.text()  # 获取源路径
+        sourcepath = self.sourcepath.text()  # 获取源路径
         if not os.path.exists(sourcepath):  # 判断是否选择了源目录
-            QMessageBox.information(self,"温馨提示：","请先选择Word文档所在的文件夹！",QMessageBox.Yes)
+            QMessageBox.information(self, "温馨提示：", "请先选择Word文档所在的文件夹！", QMessageBox.Yes)
             return
         targetpath = os.path.join(sourcepath, "pdf")  # 根据源路径生成目标目录
         if not os.path.exists(targetpath):  # 判断目录是否存在，不存在则创建
-            os.makedirs(targetpath) # 创建目录
+            os.makedirs(targetpath)  # 创建目录
         valueList = wordtopdf.wordtopdf(filelist, targetpath)
-        if(valueList != -1):
+        if (valueList != -1):
             mergepdf.mergefiles(targetpath, 'merged.pdf', True)  # 合并PDF
-            temp = [os.path.join(targetpath , 'merged.pdf')] # 生成合并后的PDF文件的路径
-            for file in valueList: # 遍历临时生成的PDF文件列表
+            temp = [os.path.join(targetpath, 'merged.pdf')]  # 生成合并后的PDF文件的路径
+            for file in valueList:  # 遍历临时生成的PDF文件列表
                 os.remove(file)  # 删除PDF文件
-            isList = self.checkBox.isChecked()    # 指定是否带目录
-            resultvalue=wordtopdf.getPdfOutlines(temp[0],targetpath,isList)   # 提取目录
-            os.remove(temp[0]) # 删除合并后的PDF文件
+            isList = self.checkBox.isChecked()  # 指定是否带目录
+            resultvalue = wordtopdf.getPdfOutlines(temp[0], targetpath, isList)  # 提取目录
+            os.remove(temp[0])  # 删除合并后的PDF文件
             if valueList != []:
                 self.listfile.clear()  # 转换完毕就将等待gif图片清理掉
             self.listfile.setText(resultvalue)  # 将生成的目录文件路径显示到页面中
-
 
     # 自定义打开子窗体的方法
     def open(self):
@@ -244,28 +251,27 @@ class ListWindow(QMainWindow, Ui_ListWindow):
     # 打开文件按钮触发的事件函数
     def openButtonClick(self):
         if self.listfile.text() == "还未提取...":
-            QMessageBox.information(self,"温馨提示：","还没有提取目录，请先单击【开始提取】按钮！",QMessageBox.Yes)
+            QMessageBox.information(self, "温馨提示：", "还没有提取目录，请先单击【开始提取】按钮！", QMessageBox.Yes)
         else:
             os.startfile(self.listfile.text())  # 打开文件
 
+
 if __name__ == '__main__':
-    app = QApplication(sys.argv)   # 创建GUI对象
-    main = MyMainWindow()   # 创建主窗体ui类对象
+    app = QApplication(sys.argv)  # 创建GUI对象
+    main = MyMainWindow()  # 创建主窗体ui类对象
     qmovie = QtGui.QMovie('image/loding.gif')
 
     transformWindow = TransformWindow()  # 创建Word转PDF窗体对象
     transformWindow.gif = qmovie  # 加载gif图片
-    main.actionWord_PDF.triggered.connect(transformWindow.open)  #为Toolbar上的Word转PDF按钮指定连接槽函数
+    main.actionWord_PDF.triggered.connect(transformWindow.open)  # 为Toolbar上的Word转PDF按钮指定连接槽函数
 
     pagewindow = PageWindow()  # 创建统计Word文档页码窗体对象
     pagewindow.gif = qmovie  # 加载gif图片
-    main.action_Word.triggered.connect(pagewindow.open)  #为Toolbar上的统计Word文档页码按钮指定连接槽函数
+    main.action_Word.triggered.connect(pagewindow.open)  # 为Toolbar上的统计Word文档页码按钮指定连接槽函数
 
     listwindow = ListWindow()  # 创建提取总目录窗体对象
     listwindow.gif = qmovie  # 加载gif图片
-    main.action_list.triggered.connect(listwindow.open)  #为Toolbar上的提取总目录按钮指定连接槽函数
+    main.action_list.triggered.connect(listwindow.open)  # 为Toolbar上的提取总目录按钮指定连接槽函数
 
     main.show()  # 显示主窗体
-    sys.exit(app.exec_())   # 除非退出程序关闭窗体，否则一直运行
-
-
+    sys.exit(app.exec_())  # 除非退出程序关闭窗体，否则一直运行
