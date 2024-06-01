b757a95d34b992b364648eee7dcf0728f9a15362
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-06-01 10:42:48 +0800

    代码优化

--- 其他/wordhelper/wordhelper.py
+++ 其他/wordhelper/wordhelper.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtGui import QColor, QMovie
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QColor, QMovie, QIcon
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QTableWidgetItem, QTableWidget, QMessageBox, QPushButton, QHBoxLayout, QWidget, QVBoxLayout
 import os
 import _thread
@@ -10,6 +11,36 @@ from win_three import Ui_MainWindow
 from win_three import Ui_PageWindow
 from win_three import Ui_ListWindow
 from transformWindow import Ui_TransformWindow
+from PyQt5.Qt import QPixmap
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QVBoxLayout, QPushButton, QLabel
+
+
+class ButtonImage(QPushButton):
+    def __init__(self, name_label, image_url):
+        super().__init__()
+        self.initUI(name_label, image_url)
+
+    def initUI(self, name_label, image_url):
+        self.resize(300, 300)
+        # 使用内部布局
+        self.layout = QVBoxLayout(self)
+        # 设置标签居中
+        alignment = Qt.AlignHCenter | Qt.AlignVCenter
+        # 创建并设置名称标签
+        self.name_label = QLabel(name_label)
+        self.name_label.setAlignment(alignment)
+        # 创建并设置图片标签
+        self.image_label = QLabel()
+        pixmap = QPixmap(image_url)
+        self.image_label.setPixmap(pixmap)
+        self.image_label.setAlignment(alignment)
+        # 将标签添加到布局中
+        self.layout.addWidget(self.image_label)
+        self.layout.addWidget(self.name_label)
+
+
+
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
@@ -19,10 +50,19 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.setWindowTitle('文档处理系统')
         self.centralWidget = QWidget(self)
         self.setCentralWidget(self.centralWidget)
+
         # Create buttons
-        self.wordPdfButton = QPushButton('转为pdf文件', self)
-        self.pageCountButton = QPushButton('统计文档页数', self)
-        self.extractListButton = QPushButton('提取文档目录', self)
+        self.wordPdfButton = ButtonImage('转为pdf文件', "pdf文件.png")
+        self.pageCountButton = ButtonImage('统计文档页数', "页数.png")
+        self.extractListButton = ButtonImage('提取文档目录', "目录.png")
+
+
+        # Set icon size
+        self.wordPdfButton.setFixedSize(200, 200)
+        self.pageCountButton.setFixedSize(200, 200)
+        self.extractListButton.setFixedSize(200, 200)
+
+
 
         # Connect buttons to methods
         self.wordPdfButton.clicked.connect(self.openTransformWindow)
