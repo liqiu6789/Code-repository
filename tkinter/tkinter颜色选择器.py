ead51e5749c08013bf8148d82e21358832f9a592
li_qiu_jieqi <13757505+li-qiu-jieqi@user.noreply.gitee.com>
2024-06-09 01:12:03 +0800

    更新

--- tkinter/tkinter颜色选择器.py
+++ tkinter/tkinter颜色选择器.py
@@ -10,15 +10,24 @@ main_application.title("颜色选择器示例")
 text_editor = tk.Text(main_application, wrap='word', relief=tk.FLAT)
 text_editor.pack(fill=tk.BOTH, expand=True)
 
+# 定义更改字体颜色的函数
+def change_font_color():
+    color = colorchooser.askcolor()[1]  # 获取选中的颜色
+    if color:
+        text_editor.config(fg=color)  # 更改文本编辑器的字体颜色
+
 # 定义更改背景颜色的函数
 def change_bg_color():
     color = colorchooser.askcolor()[1]  # 获取选中的颜色
     if color:
         text_editor.config(bg=color)  # 更改文本编辑器的背景颜色
 
-# 创建一个按钮来调用颜色选择器并放在中间
-color_button = tk.Button(main_application, text='选择背景颜色', command=change_bg_color)
-color_button.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+# 创建一个按钮来调用颜色选择器
+font_color_button = tk.Button(main_application, text='选择字体颜色', command=change_font_color)
+font_color_button.pack(pady=10)
+
+bg_color_button = tk.Button(main_application, text='选择背景颜色', command=change_bg_color)
+bg_color_button.pack(pady=10)
 
 # 运行主循环
 main_application.mainloop()
